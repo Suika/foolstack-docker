8ae5e7cb5375762ef90b647b3bc361a5c3eb2cf7
suika <2320837+Suika@users.noreply.github.com>
2019-09-08 20:11:47 +0200

    VAR namespace adjustment

--- eve/entrypoint.sh
+++ eve/entrypoint.sh
@@ -5,12 +5,12 @@ set -xe
 USER_ID=${UID}
 GROUP_ID=${GID}
 
-export EVE_BOARDS=$(echo $EVE_BOARDS | sed "s/^/'/;s/$/'/;s/\s*,\s*/','/g")
+export SCRAPER_BOARDS=$(echo $SCRAPER_BOARDS | sed "s/^/'/;s/$/'/;s/\s*,\s*/','/g")
 
 envsubst < /eve/.config.py.env > /eve/config.py
 
 echo "Setting permissions to UID/GID: ${USER_ID}/${GROUP_ID}"
 chown ${USER_ID}:${GROUP_ID} -R /eve
-chown ${USER_ID}:${GROUP_ID} ${EVE_IMGDIR}
+chown ${USER_ID}:${GROUP_ID} ${SCRAPER_IMGDIR}
 
 exec su-exec ${USER_ID}:${GROUP_ID} python3 /eve/eve.py $@
