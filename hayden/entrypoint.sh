63571f2b0be40b8803d18cadb6da5887d9da8fb8
suika <2320837+Suika@users.noreply.github.com>
2019-09-09 01:33:47 +0200

    derp

--- hayden/entrypoint.sh
+++ hayden/entrypoint.sh
@@ -7,10 +7,26 @@ GROUP_ID=${GID}
 
 export SCRAPER_BOARDS=$(echo $SCRAPER_BOARDS | sed "s/^/\"/;s/$/\"/;s/\s*,\s*/\",\"/g")
 
+if [ -z "$SCRAPER_PROXY" ]; then
+    export SCRAPER_PROXY_CONF=""
+else
+    export SCRAPER_PROXY_CONF=$( envsubst <<\EOF
+  "proxies" : [
+      {
+          "type" : "${SCRAPER_PROXY_TYPE}",
+          "url" : "${SCRAPER_PROXY_URL}",
+          "username" : "${SCRAPER_PROXY_USER}",
+          "password" : "${SCRAPER_PROXY_PASS}"
+      }
+  ],
+EOF
+)
+fi
+
 envsubst < /hayden/.config.json.env > /hayden/config.json
 
 echo "Setting permissions to UID/GID: ${USER_ID}/${GROUP_ID}"
 chown ${USER_ID}:${GROUP_ID} -R /hayden
 chown ${USER_ID}:${GROUP_ID} ${SCRAPER_IMGDIR}
 
-exec su-exec ${USER_ID}:${GROUP_ID} /hayden $@
+exec su-exec ${USER_ID}:${GROUP_ID} "$@"
