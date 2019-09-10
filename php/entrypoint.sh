cef0dcb916dfd48fccf12594d67c1fb172ce1819
suika <2320837+Suika@users.noreply.github.com>
2019-09-11 01:11:52 +0200

    Fix: trip salt, cache
    fix secure_tripcode_salt generation of first boot, since fooka does nothing
    reduce number of layers
    add option between redis and dummy cache

--- php/entrypoint.sh
+++ php/entrypoint.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
-if [ "$REDIS_ENABLE" = true ] && [ ! -z $REDIS_SERVERS ]; then
-    envsubst < /.cache.php.env > /var/www/foolfuuka/app/foolz/foolframe/config/cache.php
-    chown www:www /var/www/foolfuuka/app/foolz/foolframe/config/cache.php
-else
-    cat /.cache.php.default > /var/www/foolfuuka/app/foolz/foolframe/config/cache.php
+if [ ! -f "/var/www/foolfuuka/app/foolz/foolfuuka/config/config.php" ]; then
+    echo "$FILE exist"
+    export SECURE_TRIPCODE_SALT=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 500 | base64 | sed "s/^/'/g;s/$/'\./g;$ s/.$//")
+    envsubst < /.config.php.env > /var/www/foolfuuka/app/foolz/foolfuuka/config/config.php
 fi
+
+if [ "$REDIS_TYPE" != "redis" ] ; then
+    unset REDIS_SERVERS
+    export REDIS_TYPE=dummy
+fi
+
+envsubst < /.cache.php.env > /var/www/foolfuuka/app/foolz/foolframe/config/cache.php
+chown www:www /var/www/foolfuuka/app/foolz/foolframe/config/cache.php
+chown www:www /var/www/foolfuuka/app/foolz/foolfuuka/config/config.php
 
 "$@"
