4c5754551f20748612178de34aca7d92dcec6bb2
suika <2320837+Suika@users.noreply.github.com>
2019-12-18 02:32:53 +0100

    Ubuntu build

--- hayden/entrypoint.sh
+++ hayden/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+set -xe
+
+if hash su-exec 2>/dev/null; then
+  export RUNEXEC=su-exec
+else
+  export RUNEXEC=gosu
+fi
+
 USER_ID=${UID}
 GROUP_ID=${GID}
 
@@ -29,4 +37,4 @@ echo "Setting permissions to UID/GID: ${USER_ID}/${GROUP_ID}"
 chown ${USER_ID}:${GROUP_ID} -R /hayden
 chown ${USER_ID}:${GROUP_ID} ${SCRAPER_IMGDIR}
 
-exec su-exec ${USER_ID}:${GROUP_ID} "$@"
+exec ${RUNEXEC} ${USER_ID}:${GROUP_ID} "$@"
